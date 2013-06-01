53a5a7d372db4332d83d3ae8ebdd1760102895a7
Salil Wadnerkar <rohshall@gmail.com>
2013-06-01 10:27:23 +0800

    jsonized the API response

--- server.rkt
+++ server.rkt
@@ -4,11 +4,11 @@
 (require db)
 (require db/util/datetime)
 (require srfi/19)
+(require json)
 
 (define c
   (virtual-connection 
    (lambda ()
-     (printf "connecting!")
      (postgresql-connect #:server "localhost"
                       #:port 5432
                       #:database "sd_ventures_development"
@@ -19,8 +19,8 @@
 (get "/api/1/devices"
   (lambda (req)
     (let* ([recs (query-rows c "SELECT mac_addr FROM devices")]
-          [mac-addrs (map (lambda (rec) (vector-ref rec 0)) recs)])
-      (foldl (lambda (response mac-addr) (string-append response "\n" mac-addr)) "" mac-addrs))))
+          [mac-addrs (map (lambda (rec) (make-hasheq (list (cons 'mac-addr (vector-ref rec 0))))) recs)])
+      (jsexpr->string mac-addrs))))
 
 
 (get "/api/1/devices/:device_id"
@@ -28,18 +28,23 @@
     (let* ([device-mac-addr (params req 'device_id)]
            [rec (query-maybe-row c "SELECT manufactured_at FROM devices WHERE mac_addr = $1" device-mac-addr)])
       (if rec
-        (string-append "Device " device-mac-addr " manufactured at: " (date->string (sql-datetime->srfi-date (vector-ref rec 0))))
-        "Device does not exist"))))
+          (let ([manufactured-at (date->string (sql-datetime->srfi-date (vector-ref rec 0)))])
+            (jsexpr->string (make-hasheq (list (cons 'mac-addr device-mac-addr) (cons 'manufactured-at manufactured-at)))))
+          "Device does not exist"))))
 
 
 (get "/api/1/devices/:device_id/readings"
   (lambda (req)
     (let* ([device-mac-addr (params req 'device_id)]
            [rec (query-maybe-row c "SELECT 1 FROM devices WHERE mac_addr = $1" device-mac-addr)])
+      ; if the device record exists, only then get the readings
       (if rec
           (let* ([reading-recs (query-rows c "SELECT value, created_at FROM readings WHERE device_mac_addr = $1" device-mac-addr)]
-                 [rec-strings (map (lambda (rec) (string-append "value: " (vector-ref rec 0) " created at: " (date->string (sql-datetime->srfi-date (vector-ref rec 1))))) reading-recs)])
-            (foldl (lambda (response rec-string) (string-append response "\n" rec-string)) "" rec-strings))
+                 [rec-strings (map (lambda (rec)
+                                     (make-hasheq (list (cons 'value (vector-ref rec 0))
+                                                        (cons 'created-at (date->string (sql-datetime->srfi-date (vector-ref rec 1)))))))
+                                   reading-recs)])
+            (jsexpr->string rec-strings))
           "Device does not exist"))))
 
 (run)
